b561d3f56b038972ad7bd1d19e5c0cbe92a6978f
anmun <gummy.aja@gmail.com>
2024-07-10 22:53:09 +0200

    Added input prompt for dates in python file

--- datagen.py
+++ datagen.py
@@ -1,2 +1,21 @@
 import sqlite3
+import pandas as pd
+from datetime import datetime
 
+conn = sqlite3.connect('schemagen.sql')
+cursor = conn.cursor()
+
+while True:
+    try:
+        start_date, end_date = [
+            datetime.strptime(date_str, '%Y-%m-%d')
+            for date_str in input("Enter starting and ending dates for traffic data (YYYY-MM-DD YYYY-MM-DD): ").split()
+        ]
+
+        if start_date_obj > end_date_obj:
+            print("INVALID INPUT: start date must be before or same as end date")
+        else:
+            break
+
+    except ValueError:
+        print("INVALID INPUT: formatting error")
