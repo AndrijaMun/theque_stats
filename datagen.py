228d5dda798efab7ac2bc87bc3917508bf32b8a4
anmun <gummy.aja@gmail.com>
2024-07-13 17:32:09 +0200

    Fixed for _ in range for datetime insertion range
    Realised that the hours, minutes and seconds overflow and exclude the end_date from the loop's range

--- datagen.py
+++ datagen.py
@@ -3,6 +3,7 @@ import pandas as pd
 import random
 from datetime import datetime, timedelta
 
+# generates a random time
 def random_time():
     hour = random.randint(12, 22)
     if hour == 22:
@@ -12,9 +13,11 @@ def random_time():
     second = random.randint (0, 59)
     return hour, minute, second
 
+# connecting to the database
 conn = sqlite3.connect('theque.db')
 cursor = conn.cursor()
 
+# asks for the range of dates the user wants to generate data for
 while True:
     try:
         start_date, end_date = [
@@ -30,12 +33,14 @@ while True:
     except ValueError:
         print("INVALID INPUT: formatting error")
 
+# inserts and sorts order dates and times
 date = start_date
 while date <= end_date:
     for _ in range(1, random.randint(4, 79)):
         hour, minute, second = random_time()
         date = datetime.combine(date, datetime.min.time()).replace(hour=hour, minute=minute, second=second)
-        cursor.execute("""INSERT INTO Orders (OrderTime) VALUES (?)""", (date,))
+        cursor.execute("""INSERT INTO Orders (OrderTime, OrderTypeID, StampCouponAmount) VALUES (?, ?, ?)""", (date, random.randint(1, 4), random.randint(1, 2)))
+    date = date.replace(hour=0, minute=0, second=0)
     date += timedelta(days=1)
 cursor.execute("""SELECT DISTINCT OrderTime FROM Orders ORDER BY OrderTime""")
 sorted_data = [row[0] for row in cursor.fetchall()]
@@ -43,3 +48,7 @@ cursor.execute("""DELETE FROM Orders""")
 for row in sorted_data:
     cursor.execute("""INSERT INTO Orders (OrderTime) VALUES (?)""", (row,))
 conn.commit()
+
+
+
+               
